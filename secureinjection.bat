48f7cf4b203d06444746c72fa57a590d1d9d0b34
AnonXCSGO <43065121+AnonXCSGO@users.noreply.github.com>
2018-11-07 18:13:06 -0500

    Add files via upload

---
+++ secureinjection.bat
@@ -0,0 +1,30 @@
+@echo off
+title Secure Injection (by AnonX) [Private Source]
+
+:secure
+echo Command Terminal [Version 8.1.7]
+echo (c) 2018 AnonX. All Rights Reserved [Private Source]
+rem using "system" import BlackSmurfInjector
+echo.
+echo BlackSmurfInjector\...
+echo.
+echo Command$
+ping localhost -n 3 >nul
+rem In Command (run) "Secure"
+echo Command$ Run "Secure"
+echo.
+rem return()
+echo Running "Secure" for "BlackSmurfInjector"
+ping localhost -n 2 >nul
+echo.
+echo Found (MM Injection) from "BlackSmurfInjector"
+echo Using Code Type %random%
+ping localhost -n 4 >nul
+echo Creating Junk
+ping localhost -n 1 >nul
+echo Junk Ready
+echo.
+echo INJECTION SECURED BY SECURE INJECTION BY ANONX!
+echo.
+echo (Press any key to exit program)
+pause>nul
